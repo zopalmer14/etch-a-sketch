e5db9410c5904cbd7ca992c1c2227fdc1b7b5d2e
zopalmer14 <zopalmer14@gmail.com>
2024-01-28 15:44:25 -0500

    refactor: make tile sizing dynamic based on grid dimensions

--- dynamic.js
+++ dynamic.js
@@ -12,25 +12,37 @@ reset_btn.addEventListener('click', () => {
 
     // delete the current grid, then create a new one with the specified dimensions
     grid_container.replaceChildren();
-    createGrid(grid_dim, grid_dim);
+    createGrid(grid_dim);
 });
 
 // GRID CREATION
 
-let tileStyle = `
-    background-color: white; 
-    margin: 0.25vh; 
-    width: 4vh; 
-    height: 4vh;
+let columnStyle = `
+    display: flex;
+    flex-direction: column;
+    margin: 0px 1px;
+    flex: 1 1 0;
 `;
 
-function createGrid(numRows, numCols) {
-    for (let col = 0; col < numCols; col++) {
-        for (let row = 0; row < numRows; row++) {
-            // create a tile, assign the css styling, then append to the grid
+let tileStyle = `
+    background-color: white; 
+    margin: 1px 0px;
+    flex: 1 1 0;
+`;
+
+function createGrid(grid_dim) {
+    for (let col = 0; col < grid_dim; col++) {
+        // create a column flex-container for the tiles
+        const column = document.createElement('div');
+        column.style.cssText = columnStyle;
+        grid_container.appendChild(column);
+
+        // create tiles for each of the column containers
+        for (let row = 0; row < grid_dim; row++) {
+            // create a tile, assign the css styling, then append to the column
             const tile = document.createElement('div');
             tile.style.cssText = tileStyle;
-            grid_container.appendChild(tile);
+            column.appendChild(tile);
 
             // create an eventListener to change the color of the tile when moused-over 
             tile.addEventListener('mouseover', function (e) {
@@ -40,4 +52,4 @@ function createGrid(numRows, numCols) {
     }
 }
 
-createGrid(16, 16);
+createGrid(16);
