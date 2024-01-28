c16b33a68411325a4bee603453346c9a7c3c6f69
zopalmer14 <zopalmer14@gmail.com>
2024-01-28 14:40:55 -0500

    added 'sketching' functionality, making tile color change when moused-over

--- dynamic.js
+++ dynamic.js
@@ -1,12 +1,25 @@
 
 const grid_container = document.querySelector('#grid_container');
 
+let tileStyle = `
+    background-color: white; 
+    margin: 0.25vh; 
+    width: 4vh; 
+    height: 4vh;
+`;
+
 function createGrid(numRows, numCols) {
     for (let col = 0; col < numCols; col++) {
         for (let row = 0; row < numRows; row++) {
+            // create a tile, assign the css styling, then append to the grid
             const tile = document.createElement('div');
-            tile.style.cssText = 'background-color: white; margin: 0.25vh; width: 4vh; height: 4vh;';
+            tile.style.cssText = tileStyle;
             grid_container.appendChild(tile);
+
+            // create an eventListener to change the color of the tile when moused-over 
+            tile.addEventListener('mouseover', function (e) {
+                e.target.style.backgroundColor = 'lightgray';
+            });
         }
     }
 }
